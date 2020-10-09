f9ecbb88ce23bb45a5f4a7f468d9386aed768953
Kean Maizels <kean@kean.com.au>
2020-10-09 17:58:57 +1100

    Revert "Minor fix re serial numbers"
    This reverts commit d6ad231f2ca751d9f97363872ab277c8c541bb2c.

--- BattLab_One_V1_0_9.py
+++ BattLab_One_V1_0_9.py
@@ -390,7 +390,7 @@ ports = list(serial.tools.list_ports.comports())
 for p in ports:
    
    if p.vid == 0x0403 and p.pid == 0x6001:
-      ser_num_prefix = p.serial_number[:2]
+      ser_num_prefix = p.serial_number[0] + p.serial_number[1]
       if ser_num_prefix == 'BB':
          com_port = list(list_ports.grep("0403:6001"))[0][0]       
          init(com_port)
